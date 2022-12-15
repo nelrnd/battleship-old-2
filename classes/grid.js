fd98ef2155ad8b5b991ea2d10a300caecb04dd82
Nel <nel.renaudin@gmail.com>
2022-12-15 17:21:31 +0100

    Turn factory function into class, write and pass tests

--- classes/grid.js
+++ classes/grid.js
@@ -1,18 +1,19 @@
 let gridSize = 10;
 
-const Grid = (size = gridSize) => {
-  const grid = [];
-  const placedShips = [];
-
-  // populate grid with squares
-  for (let i = 0; i < size * size; i++) {
-    const square = {};
-    square.x = i % size;
-    square.y = Math.floor(i / size);
-    grid.push(square);
+export class Grid {
+  constructor(size) {
+    this.grid = this.createGrid(size);
+    this.placedShips = [];
   }
 
-  return { grid };
-};
-
-export { Grid };
+  createGrid(size = gridSize) {
+    const grid = [];
+    for (let i = 0; i < size * size; i++) {
+      const square = [];
+      square.x = i % size;
+      square.y = Math.floor(i / size);
+      grid.push(square);
+    }
+    return grid;
+  }
+}
--- tests/grid.test.js
+++ tests/grid.test.js
@@ -1,11 +1,11 @@
 import { Grid } from '../classes/grid.js';
 
 test('Creating grid', () => {
-  const grid = Grid();
+  const grid = new Grid();
   expect(grid.grid.length).toBe(100);
 });
 
 test('Creating 6x6 grid', () => {
-  const grid = Grid(6);
+  const grid = new Grid(6);
   expect(grid.grid.length).toBe(36);
 });
