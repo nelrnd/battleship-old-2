d6f192a9104bef5ce9d894c5c528654ee8ef080f
Nel <nel.renaudin@gmail.com>
2022-12-15 17:32:01 +0100

    Add findSquare and linkSquares method to Grid class

--- classes/grid.js
+++ classes/grid.js
@@ -9,11 +9,27 @@ export class Grid {
   createGrid(size = gridSize) {
     const grid = [];
     for (let i = 0; i < size * size; i++) {
-      const square = [];
+      const square = {};
       square.x = i % size;
       square.y = Math.floor(i / size);
       grid.push(square);
     }
+    this.linkSquares(grid);
     return grid;
+  }
+
+  findSquare(x, y, grid = this.grid) {
+    return grid.find((square) => square.x === x && square.y === y);
+  }
+
+  linkSquares(grid) {
+    for (const square of grid) {
+      const x = square.x;
+      const y = square.y;
+      square.top = this.findSquare(x, y - 1, grid);
+      square.right = this.findSquare(x + 1, y, grid);
+      square.bottom = this.findSquare(x, y + 1, grid);
+      square.left = this.findSquare(x - 1, y, grid);
+    }
   }
 }
--- tests/grid.test.js
+++ tests/grid.test.js
@@ -9,3 +9,19 @@ test('Creating 6x6 grid', () => {
   const grid = new Grid(6);
   expect(grid.grid.length).toBe(36);
 });
+
+test('Find a square', () => {
+  const grid = new Grid();
+  const square = grid.findSquare(4, 6);
+  expect(square.x).toBe(4);
+  expect(square.y).toBe(6);
+});
+
+test('linSquares(), getting bottom square of a square', () => {
+  const grid = new Grid();
+  const square = grid.findSquare(5, 5);
+  expect(square.top.y).toBe(4);
+  expect(square.right.x).toBe(6);
+  expect(square.bottom.y).toBe(6);
+  expect(square.left.x).toBe(4);
+});
