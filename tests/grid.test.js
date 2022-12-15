24b77db8fb78eeef563267107992a86969980848
Nel <nel.renaudin@gmail.com>
2022-12-15 16:13:12 +0100

    Write tests for creating grid

--- tests/grid.test.js
+++ tests/grid.test.js
@@ -1,6 +1,11 @@
 import { Grid } from '../classes/grid.js';
 
 test('Creating grid', () => {
-  const grid = Grid(10);
+  const grid = Grid();
   expect(grid.grid.length).toBe(100);
+});
+
+test('Creating 6x6 grid', () => {
+  const grid = Grid(6);
+  expect(grid.grid.length).toBe(36);
 });
